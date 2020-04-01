e09d2867c03f3bc9dfbf4a94a57ea97ecc3fa37e
Nicole Aranzanso <naranzanso@Nicoles-MacBook-Pro.local>
2020-04-01 03:11:30 -0400

    adding more modals

--- js/index.js
+++ js/index.js
@@ -66,25 +66,25 @@ carouselSide.addEventListener('transitionend',() => {
   })
 });
 
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+let openModalButtons = document.querySelectorAll('[data-modal-target]')
+let closeModalButtons = document.querySelectorAll('[data-close-button]')
+let overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
+    let modal = document.querySelector(button.dataset.modalTarget)
     openModal(modal)
   })
 })
 overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
+  let modals = document.querySelectorAll('.modal.active')
   modals.forEach(modal => {
     closeModal(modal)
   })
 })
 closeModalButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
+    let modal = button.closest('.modal')
     closeModal(modal)
   })
 })
