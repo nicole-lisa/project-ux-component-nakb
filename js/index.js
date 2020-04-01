cb4fae212f1d104801e644924b884632d0c3e32a
Nicole Aranzanso <naranzanso@Nicoles-MacBook-Pro.local>
2020-04-01 00:39:06 -0400

    Modal for pictures

--- js/index.js
+++ js/index.js
@@ -18,6 +18,8 @@ window.addEventListener(`scroll`, event => {
 
   console.log(percentageScrolled);
 
+  
+
 // Component 2 : Carousel slide
 
 const carouselSide = document.querySelector('.carousel-slide');
@@ -63,3 +65,36 @@ carouselSide.addEventListener('transitionend',() => {
   }
   })
 });
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
