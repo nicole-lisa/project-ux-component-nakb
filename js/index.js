88b2fa36e59faf72e6ebc5e5591f4bf2cd4888f1
Karen Bernal <karenbernal@Karens-MacBook-Pro.local>
2020-04-01 23:47:42 -0400

    LoaderVideo

--- js/index.js
+++ js/index.js
@@ -161,3 +161,23 @@ span6.onclick = function() {
   modal6.style.display = "none";
 }
 
+
+// Get the modal
+var modal7 = document.getElementById("myModal7");
+
+// Get the button that opens the modal
+var btn7 = document.getElementById("myBtn7");
+
+// Get the <span> element that closes the modal
+var span7 = document.getElementsByClassName("close7")[0];
+
+// When the user clicks the button, open the modal 
+btn7.onclick = function() {
+  modal7.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span7.onclick = function() {
+  modal7.style.display = "none";
+}
+
